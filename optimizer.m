e7ca538a2e45fec1bc0f1a0df9a4890d7e9ea362
Joel Linder <joelsc.linder@gmail.com>
2023-02-08 12:36:06 +0100

    Work on rectangular flow opti with ga

--- optimizer.m
+++ optimizer.m
@@ -1,3 +1,4 @@
+%% Symmetrical pipe
 model = mphload("models/symmetrical-pipe.mph");
 model.param.set('S_R', 0.2)
 model.study('std1').run
@@ -6,3 +7,25 @@ model.study('std1').run
 
 display(Q1)
 display(Q2)
+
+%% Rectangular Channel 
+model = mphload('models/rectangular-channel.mph');
+
+A = 0;
+b = 0;
+lb = 0.0001;
+ub = 0.9999;
+
+obj = @(x) fit(x, model);
+x = ga(obj, 1, A, b);
+
+
+function y = fit(x, model) 
+    model.param.set('B_W', x(1));
+    model.study('std1').run
+
+    Q1 = mphmean(model,'spf.U', 'volume');
+    display(Q1)
+
+    y = Q1;
+end
