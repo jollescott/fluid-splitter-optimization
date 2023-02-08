c78494b367ff485f4560762c116ab0fc64d01668
Joel Linder <joelsc.linder@gmail.com>
2023-02-08 11:10:51 +0100

    Added simple study example

--- optimizer.m
+++ optimizer.m
@@ -1,2 +1,8 @@
 model = mphload("models/symmetrical-pipe.mph");
-mphgeom(model);
+model.param.set('S_R', 0.2)
+model.study('std1').run
+
+[Q1, Q2] = mphint2(model,{'spf.T_stressz', 'spf.T_stress_tensorzz'}, 'line', 'selection',[6,7]);
+
+display(Q1)
+display(Q2)
