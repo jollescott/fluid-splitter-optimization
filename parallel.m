4478307e4e1844a85d0b479d31479e54c8388a85
Tixoj <christoffer.palmer@gmail.com>
2023-02-22 14:23:54 +0100

    Diverse ändringar

--- parallel.m
+++ parallel.m
@@ -1,70 +1,72 @@
-clear all
-
-np = 8;
-nw = 3;
-
-pool = gcp('nocreate');
-
-if ~isempty(pool)
-    delete(pool)
-end
-
-pool = parpool(nw);
-
-comsol = getComsolExec();
-
-for i = 1:nw
-    port = 2035 + i;
-    cmd = comsol + ' -autosave off -np ' + num2str(np) + ' -multi on -silent -port ' + num2str(port) + ' &';
-    system(cmd);
-
-    pause(5);
-end
-
-ga_options = optimoptions('ga', 'PopulationSize', 10, 'MaxGenerations', 5, 'UseParallel',true);
-
-tic;
-
-x = ga(@(x) parallelFit(x), 2, [], [], [1 1], 10, [0.5 0.5], [9.5 9.5], [], ga_options);
-
-f = parfevalOnAll(pool, @disconnect, 0);
-wait(f);
-
-if isunix
-    system('pkill -f comsol');
-end
-
-delete(pool);
-
-pt = toc;
-
-disp('Solution:')
-disp(x)
-disp(pt);
-
-function y = parallelFit(x) 
-
-    global model
-
-    wId = get(getCurrentTask, 'ID');
-
-    if isempty(model) 
-        if isempty(wId)
-            port = 2036;
-        else
-            port = 2035 + wId;
+function parallel(model_name, fit_function) 
+    clear all
+    
+    np = 8;
+    nw = 3;
+    
+    pool = gcp('nocreate');
+    
+    if ~isempty(pool)
+        delete(pool)
+    end
+    
+    pool = parpool(nw);
+    
+    comsol = getComsolExec();
+    
+    for i = 1:nw
+        port = 2035 + i;
+        cmd = comsol + ' -autosave off -np ' + num2str(np) + ' -multi on -silent -port ' + num2str(port) + ' &';
+        system(cmd);
+    
+        pause(5);
+    end
+    
+    ga_options = optimoptions('ga', 'PopulationSize', 10, 'MaxGenerations', 5, 'UseParallel',true);
+    
+    tic;
+    
+    x = ga(@(x) parallelFit(x), 2, [], [], [1 1], 10, [0.5 0.5], [9.5 9.5], [], ga_options);
+    
+    f = parfevalOnAll(pool, @disconnect, 0);
+    wait(f);
+    
+    if isunix
+        system('pkill -f comsol');
+    end
+    
+    delete(pool);
+    
+    pt = toc;
+    
+    disp('Solution:')
+    disp(x)
+    disp(pt);
+    
+    function y = parallelFit(x) 
+    
+        global model
+    
+        wId = get(getCurrentTask, 'ID');
+    
+        if isempty(model) 
+            if isempty(wId)
+                port = 2036;
+            else
+                port = 2035 + wId;
+            end
+        
+            mphstart(port)
+            model = mphload(model_name);
         end
     
-        mphstart(port)
-        model = mphload('models/rectangular-channel.mph');
+        y = fit_function(x, model);   
     end
-
-    y = rectangularfit(x, model);   
-end
-
-function disconnect() 
-    import com.comsol.model.util.*
-    ModelUtil.disconnect;
-
-    clear all
+    
+    function disconnect() 
+        import com.comsol.model.util.*
+        ModelUtil.disconnect;
+    
+        clear all
+    end
 end
--- reynoldsfit.m
+++ reynoldsfit.m
@@ -1,4 +1,16 @@
 function f = reynoldsfit(x, model)
+  model.param.set('r1' , x(1));
+    model.param.set( 'r2',x(2));
+    model.param.set('r3' , x(3));
+    model.param.set('r4' ,x(4)); 
+    model.param.set('w1' , x(5));
+    model.param.set('w2' ,x(6));
+    model.param.set('w3' , x(7));
+    model.param.set('w4', x(8));
 
+    model.study('std1').run
+
+   a = mphmean(model,'spf.cellRe', 'volume', 'selection',1);
+   y = a;
 end
 
